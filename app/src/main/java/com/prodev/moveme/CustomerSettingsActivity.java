c56642a49398ca1ad63821df2e91d2f332f84fef
pwachira900@gmail.com <pwachira900@gmail.com>
2019-06-23 20:33:42 +0300

    get user info mehtod to listen fo=

--- app/src/main/java/com/prodev/moveme/CustomerSettingsActivity.java
+++ app/src/main/java/com/prodev/moveme/CustomerSettingsActivity.java
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
 
 public class CustomerSettingsActivity extends AppCompatActivity {
 
@@ -45,5 +51,55 @@ public class CustomerSettingsActivity extends AppCompatActivity {
         userID = mAuth.getCurrentUser().getUid();
         mCustomerDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child("Customers").child(userID);
 
+        getUserInfo();
+
+        //confirm button
+        mConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveUserInformation();
+            }
+        });
+
+        //back button
+        mBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                return;
+            }
+        });
     }
+
+    // listen for changes in mCustomer then save the values
+    private void getUserInfo(){
+        mCustomerDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists() && dataSnapshot.getChildrenCount()>0){
+                    Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                    if(map.get("name")!=null){
+                        mName = map.get("name").toString();
+                        mNameField.setText(mName);
+                    }
+                    if(map.get("phone")!=null){
+                        mPhone = map.get("phone").toString();
+                        mPhoneField.setText(mPhone);
+                    }
+//                    if(map.get("profileImageUrl")!=null){
+//                        mProfileImageUrl = map.get("profileImageUrl").toString();
+//                        Glide.with(getApplication()).load(mProfileImageUrl).into(mProfileImage);
+//                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
+
+    //save user's information
+
+
 }
