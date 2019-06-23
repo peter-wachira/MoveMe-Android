1bf5f28f655ea39a4c39445d516d4c243e6060a0
pwachira900@gmail.com <pwachira900@gmail.com>
2019-06-23 20:21:28 +0300

    add customer settings xml fields and bind to class

--- app/src/main/java/com/prodev/moveme/CustomerSettingsActivity.java
+++ app/src/main/java/com/prodev/moveme/CustomerSettingsActivity.java
@@ -2,13 +2,48 @@ package com.prodev.moveme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class CustomerSettingsActivity extends AppCompatActivity {
+
+    private EditText mNameField, mPhoneField;
+    private Button mBack, mConfirm;
+    private ImageView mProfileImage;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mCustomerDatabase;
+
+    private String userID;
+    private String mName;
+    private String mPhone;
+    private String mProfileImageUrl;
+
+    private Uri resultUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_settings);
+
+
+        mNameField = (EditText) findViewById(R.id.name);
+        mPhoneField = (EditText) findViewById(R.id.phone);
+
+        mProfileImage = (ImageView) findViewById(R.id.profileImage);
+
+        mBack = (Button) findViewById(R.id.back);
+        mConfirm = (Button) findViewById(R.id.confirm);
+
+        mAuth = FirebaseAuth.getInstance();
+        userID = mAuth.getCurrentUser().getUid();
+        mCustomerDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child("Customers").child(userID);
+
     }
 }
