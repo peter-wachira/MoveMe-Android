60b03bbe264a8f8b48c3caf8531f27d7cd5e21eb
pwachira900@gmail.com <pwachira900@gmail.com>
2019-06-23 22:50:46 +0300

    add driver settings activity

---
+++ app/src/main/java/com/prodev/moveme/DriverSettingsActivity.java
@@ -0,0 +1,14 @@
+package com.prodev.moveme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DriverSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_settings);
+    }
+}
